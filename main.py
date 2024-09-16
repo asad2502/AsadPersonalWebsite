b95cad9a0bbf7d4bc0de2a5385b6a69f3d146d4e
asad2502 <cheesymeatballsoup@gmail.com>
2024-09-16 12:56:20 +0700

    updated working nav

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # you have to import flask or install if not installed already
 import flask
+from flask import render_template
 
 # make app using flask
 app = flask.Flask(__name__)
@@ -9,12 +10,25 @@ app = flask.Flask(__name__)
 
 # make function
 
-@app.route('/')
+@app.route('/extra')
 def mainpage():
     return '<h1>Good morning, world</h1>'
 
-
-
+@app.route('/')
+def landingpage():
+    return render_template('landingpage.html')
+@app.route('/about')
+def aboutpage():
+    return render_template('aboutpage.html')
+@app.route('/todolist')
+def todolist():
+    return render_template('todolist.html')
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 # run app
 if __name__ == '__main__':
